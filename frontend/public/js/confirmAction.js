f06f83fc7279db3ba12eca7f0269dab678f1fa17
BrnCalisario <bruno145224@gmail.com>
2023-02-06 08:22:58 -0300

    ajustando responsividade

--- frontend/public/js/confirmAction.js
+++ frontend/public/js/confirmAction.js
@@ -1,10 +1,20 @@
-const deleteFunction = (event) => {
+console.log()
+const deleteFunctionDevice = (event) => {
     event.preventDefault()
     if (window.confirm("Tem certeza que deseja remover este componente ?")) {
         const form = document.getElementById('deleteForm')
         form.submit()
     }
 } 
+
+const deleteFunctionComputer = (event) => {
+    event.preventDefault()
+    if (window.confirm("Tem certeza que deseja remover este computador ?")) {
+        const form = document.getElementById('deleteForm')
+        form.submit()
+    }
+} 
+
 
 const resolveLog = (event, i) => {
     event.preventDefault()
